a05c74694a4cac4bc76ddd814a8a40a71619729c
s150673 <thibo.maes@student.ap.be>
2025-05-14 15:23:56 +0200

    Remove OSM custom component

--- lib/routes/main/listing/listing_map_route.dart
+++ lib/routes/main/listing/listing_map_route.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/components/map.dart';
 
 class ListingMapRoute extends StatelessWidget {
   const ListingMapRoute({super.key});
@@ -8,8 +7,8 @@ class ListingMapRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Listing Map")),
-      body: const Center(
-        child: OpenStreetMap()
+      body: Center(
+        child: const Text("Map")
       ),
     );
   }
--- lib/routes/main/listing/listing_route.dart
+++ lib/routes/main/listing/listing_route.dart
@@ -10,7 +10,7 @@ class MainListingRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Appliances Listing")),
+      appBar: AppBar(title: const Text("Home")),
       body: Padding(
         padding: const EdgeInsets.all(8),
         child: Column(
