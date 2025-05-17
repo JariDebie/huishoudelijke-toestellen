aea9bc821abc46745b4ec4f28faf0325c7e42146
s150673 <thibo.maes@student.ap.be>
2025-05-17 14:38:02 +0200

    Add appliance class + author retrieval method

---
+++ lib/types/appliance.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_application_1/types/user.dart';
+import 'package:latlong2/latlong.dart';
+
+class Appliance {
+  final String authorId;
+  User? _author;
+  final String description;
+  final String imageUrl;
+  final double price;
+  final LatLng location;
+  final DateTime availableFrom;
+  final DateTime availableUntil;
+
+  Appliance({
+    required this.description,
+    required this.authorId,
+    required this.imageUrl,
+    required this.price,
+    required this.location,
+    required this.availableFrom,
+    required this.availableUntil
+  });
+
+  Future<User?> getAuthor() async {
+    if (_author != null) return _author;
+    CollectionReference users = FirebaseFirestore.instance
+        .collection("users")
+        .withConverter<User>(
+          fromFirestore: User.fromFirestore,
+          toFirestore: (User u, _) => u.toFirestore()
+        );
+    DocumentSnapshot userDoc = await users.doc(authorId).get();
+
+    if (userDoc.exists) {
+      User user = userDoc.data() as User;
+      _author = user;
+      return user;
+    }
+    
+    return null;
+  }
+}
